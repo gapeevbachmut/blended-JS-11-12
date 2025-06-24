63387c4818405ac5f4727cdcac8cba5b4fcbdde0
gapeevbachmut <gapeev1979@gmail.com>
2025-06-24 16:31:25 +0000

    Deploying to gh-pages from @ gapeevbachmut/blended-JS-11-12@836b78921f7624b5cf20f67251e6a2b461fd5835 🚀

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import"./assets/styles-BrE9sqBu.js";import{a as n,i as m}from"./assets/vendor-4yCzdkXl.js";const l=document.querySelector(".categories"),u=document.querySelector(".products"),i=document.querySelector(".not-found"),r="https://dummyjson.com/products",y="/category-list";async function f(){const{data:t}=await n(`${r}${y}`);return t.unshift("All"),t}async function d(t=1){const{data:s}=await n(`${r}?limit=12&skip=${(t-1)*12}`);return s.products}async function b(t,s=1){const{data:o}=await n(`${r}/category/${t}?limit=12&skip=${(s-1)*12}`);return console.log(t),o.products}function $(t){const s=t.map(o=>`
+import"./assets/styles-BrE9sqBu.js";import{a as i,i as b}from"./assets/vendor-4yCzdkXl.js";const g=document.querySelector(".categories"),p=document.querySelector(".products"),u=document.querySelector(".not-found"),a=document.querySelector(".load-more-btn"),l="https://dummyjson.com/products",h="/category-list";async function $(){const{data:t}=await i(`${l}${h}`);return t.unshift("All"),t}async function d(t=1){const{data:e}=await i(`${l}?limit=12&skip=${(t-1)*12}`);return e.products}async function _(t,e=1){const{data:o}=await i(`${l}/category/${t}?limit=12&skip=${(e-1)*12}`);return console.log(t),o.products}function k(t){const e=t.map(o=>`
     <li class="categories__item">
       <button class="categories__btn" type="button">${o}</button>
     </li>
-    `).join("");l.insertAdjacentHTML("beforeend",s)}function L(t){const s=t.map(({id:o,thumbnail:e,title:c,brand:p,category:g,price:_})=>`
+    `).join("");g.insertAdjacentHTML("beforeend",e)}function m(t){const e=t.map(({id:o,thumbnail:s,title:c,brand:y,category:f,price:L})=>`
     <li class="products__item" data-id="${o}">
-    <img class="products__image" src="${e}" alt="${c}"/>
+    <img class="products__image" src="${s}" alt="${c}"/>
     <p class="products__title">${c}</p>
-    <p class="products__brand"><span class="products__brand--bold">Brand:${p}</span></p>
-    <p class="products__category">Category: ${g}</p>
-    <p class="products__price">Price: ${_} $</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:${y}</span></p>
+    <p class="products__category">Category: ${f}</p>
+    <p class="products__price">Price: ${L} $</p>
  </li>
 
-    `).join("");u.insertAdjacentHTML("beforeend",s)}let a=1;async function k(t){if(!t.target.classList.contains("categories__btn")){console.log("not a btn");return}const s=t.target,o=t.target.textContent;document.querySelectorAll(".categories__btn--active").forEach(c=>c.classList.remove("categories__btn--active")),s.classList.add("categories__btn--active"),u.innerHTML="",a=1;let e;try{if(o==="All"?e=await d(a):e=await b(o,a),e.length)i.classList.remove("not-found--visible");else{i.classList.add("not-found--visible");return}L(e)}catch(c){console.log(c)}}l.addEventListener("click",k);async function P(){try{const t=await f();console.log(t),$(t);const s=await d()}catch(t){console.log(t),m.error({title:t.message})}}P();
+    `).join("");p.insertAdjacentHTML("beforeend",e)}let n=1,r;async function w(t){const e=t.target;if(!t.target.classList.contains("categories__btn")){console.log("not a btn");return}const o=t.target.textContent;r=o,document.querySelectorAll(".categories__btn--active").forEach(c=>c.classList.remove("categories__btn--active")),e.classList.add("categories__btn--active"),p.innerHTML="",n=1;let s=[];try{if(o==="All"?s=await d(n):s=await _(o,n),s.length)u.classList.remove("not-found--visible");else{u.classList.add("not-found--visible"),a.classList.add("hidden");return}console.log("btn-category",s),m(s),s.length===12?a.classList.remove("hidden"):a.classList.add("hidden")}catch(c){console.log(c)}}async function v(){n+=1;try{let t=[];if(r==="All"?t=await d(n):t=await _(r,n),!t.length){a.classList.add("hidden");return}m(t),console.log("btn-load-More",t),t.length<12&&a.classList.add("hidden")}catch(t){console.error(t)}}g.addEventListener("click",w);a.addEventListener("click",v);async function M(){try{const t=await $();console.log(t),k(t);const e=await d()}catch(t){console.log(t),b.error({title:t.message})}}M();
 //# sourceMappingURL=index.js.map
