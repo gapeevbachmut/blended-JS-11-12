7f235c84bbf52ec3a19bcc06507a1b34b6af985c
Андрій <gapeev1979@gmail.com>
2025-07-16 16:01:39 +0300

    fix modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,90 +5,30 @@ import { refs } from './refs';
 import { handleClickProductsCart, globalProductCartId } from './handlers';
 import { getProductsById, getProductsList } from './products-api';
 
+//
+//
+//
 // open модальне вікно, при кліку, на картку продукту, у хендлерс - handleClickProductsCart
+//
+//
 
 export function openModal() {
   //  при відкритті модалки - що робиться!!!
-  const selectProduct = globalProductCartId; // при кліку по картці визначаємо id продукту
+  const selectProduct = globalProductCartId; // при кліку по картці було визначчено id продукту - нашо???
   console.log('modal id', selectProduct);
   if (!selectProduct) {
     //  якщо id не має
     console.warn('Товар не знайдено!');
   }
-  refs.modalWindow.classList.add('modal--is-open'); //  прибрати клас та сховати модальне вікно - бекдропа
-  refs.modalContent.classList.add('modal--is-open'); // прибрати клас та сховати  контент модального вікна
+  refs.modalWindow.classList.add('modal--is-open'); //  додати клас та показати модальне вікно = бекдроп
+  refs.modalContent.classList.add('modal--is-open'); // додати клас та показати контент модального вікна
   refs.modalWindow.addEventListener('click', handleClickModalWindow); // -  слухач на модальне вікно - бекдроп
   window.addEventListener('keydown', onEscKeyPress); //  - слухaч на ескейп
   refs.modalCloseBtn.addEventListener('click', handleClickModalCloseBtn); // - слухач на закриття модалки - хрестик
 }
 
-// export function closeModal() {
-//   // ховаємо модадку
-//   refs.modalWindow.classList.remove('modal--is-open');
-//   // прибираємо слухачі
-//   window.removeEventListener('keydown', onEscKeyPress);
-//   refs.modalBackdrop.removeEventListener('click', onBackdropClick);
-// }
-
-// Закриття по Escape
-// function onEscKeyPress(event) {
-//   if (event.key === 'Escape') {
-//     closeModal();
-//   }
-// }
-
-// Закриття по кліку на бекдроп
-// function onBackdropClick(event) {
-//   if (event.target === event.currentTarget) {
-//     closeModal();
-//   }
-// }
-
-//при кліку на хрестик прибрати з модалки клас опен, додати сдухач на хрестик
-// export function handleClickModalCloseBtn(event) {
-//   refs.modalWindow.classList.remove('modal--is-open');
-//   // console.log('fun-close');
-
-//   handleClickModalCloseBtn(); // перенести
-
-//   ////////////////////////////////////////////////////////
-//   ////////////////////////////////////////////////////////
-//   ////////////////////////////////////////////////////////
-
-//   //
-//   //
-//   // додати закриття при кліку за модалкою, та ескейпом
-//   //
-//   //
-
-//   //           додавання у кошик
-
-//   //
-//   //   слухач на модалку, у функцію прописати клік по кнопці
-//   //     клік по кнопці  -  Add to cart
-//   refs.modalContent.addEventListener('click', handleClickModalContent);
-// }
-
-// function handleClickModalContent(event) {
-//   if (event.target.classList.contains('modal-product__btn--cart')) {
-//     console.log('клік по  -  Add to cart - 2');
-//   }
-
-//   const modalProduct = event.target.closest('.modal__content');
-//   if (!modalProduct) {
-//     console.warn('Елемент .modal-product не знайдено');
-//     return;
-//   }
-
-//   refs.modalWindow.classList.add('modal--is-open'); // додаванни класу та відкриття модального вікна - бекдропа
-//   refs.modalContent.classList.add('modal--is-open'); // додавання класу та відкриття контенту модального вікна
-//   refs.modalWindow.addEventListener('click', handleClickModalWindow); //   слухач на модальне вікно - бекдроп
-//   window.addEventListener('keydown', onEscKeyPress); // слухaч на ескейп
-//   refs.modalCloseBtn.addEventListener('click', handleClickModalCloseBtn); // слухач на закриття модалки - хрестик
-//   // слухачі навісити при кліку по картці у handleClickProductsCart - ???
-// }
-
 //
+// що робиться при закритті модалки
 
 function closeModalWindow() {
   refs.modalWindow.classList.remove('modal--is-open'); //  прибрати клас та сховати модальне вікно - бекдропа
@@ -98,7 +38,7 @@ function closeModalWindow() {
   refs.modalCloseBtn.removeEventListener; // ('click', handleClickModalCloseBtn); // - слухач на закриття модалки - хрестик
 }
 
-//     обробник слухача модального вікна
+//     обробник слухача модального вікна - бекдропа
 function handleClickModalWindow(event) {
   if (event.target === event.currentTarget) {
     closeModalWindow();
@@ -127,9 +67,7 @@ function handleClickModalCloseBtn(event) {
 //
 //
 
-// //
-// //
-// // додати закриття при кліку за модалкою, та ескейпом
+//
 // //
 // //
 
